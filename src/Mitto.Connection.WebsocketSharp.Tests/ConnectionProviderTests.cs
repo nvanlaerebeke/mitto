4e7c16043fc9c6e284a4ebbb59bb0a66da26ca43
Nico Van Laerebeke <nvanlaerebeke@users.noreply.github.com>
2019-11-10 11:27:45 +0100

    Finished Rename Websocket to WebsocketSharp

---
+++ src/Mitto.Connection.WebsocketSharp.Tests/ConnectionProviderTests.cs
@@ -0,0 +1,21 @@
+﻿using Mitto.IConnection;
+using NUnit.Framework;
+
+namespace Mitto.Connection.WebsocketSharp.Tests {
+
+    [TestFixture]
+    public class ConnectionProviderTests {
+
+        [Test]
+        public void CreateClient() {
+            Config.Initialize();
+            Assert.IsInstanceOf<IClient>(new WebSocketConnectionProvider().CreateClient());
+        }
+
+        [Test]
+        public void CreateServer() {
+            Config.Initialize();
+            Assert.IsInstanceOf<IServer>(new WebSocketConnectionProvider().CreateServer());
+        }
+    }
+}
